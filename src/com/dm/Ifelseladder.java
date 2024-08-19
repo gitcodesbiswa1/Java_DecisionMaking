97fe78f66fea7a3b17c66fb0185ab73d3ce5a1dc
gitcodesbiswa1 <biswajitstudy8@gmail.com>
2024-08-19 04:17:19 -0700

    First commit here

---
+++ src/com/dm/Ifelseladder.java
@@ -0,0 +1,21 @@
+package com.dm;
+
+public class Ifelseladder {
+
+	public static void main(String[] args) {
+		String city= "Jjkp";
+		if(city=="Meerut") {
+			System.out.println("city is meerut");
+		}
+		else if(city=="Puri") {
+			System.out.println("city is puri");
+		}
+		else if(city=="Hyderabad"){
+			System.out.println("city is hyd");
+		}
+		else {
+			System.out.println(city);
+		}
+	}
+
+}
